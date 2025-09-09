a9714d50018096b7d809f65e4f95f1b570967469
Hutansh <hutanshsharma241005@gmail.com>
2025-09-10 00:44:19 +0530

    changed secant

--- Nm lab assignment/Lab 2/secant.cpp
+++ Nm lab assignment/Lab 2/secant.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -11,22 +10,21 @@ double f(double x){
     return (so-(m*g*x)/k+(pow(m,2)*g*(1-exp(-k*x/m)))/pow(k,2));
 }
 
-float secant(double a, double b) {
-    int number_of_iter=10000;
-    while(number_of_iter){
-        if(f(a)==f(b)){
-            cout<<"Division by zero"<<endl;
-            break;
+double secant(double a,double b,int iter,double tol){
+    for(int i=0;i<iter;i++){
+        double df = (f(a)-f(b))/(a-b);
+        if(df==0){
+            cout<<"Zero derivative";
+            return -1;
         }
-        float temp=a-(f(a)*(a-b))/(f(a)-f(b));
+        double newpt = a-f(a)/df;
+        if(fabs(newpt-a)<tol) return newpt;
         b=a;
-        a=temp;
-        number_of_iter--;
-        if(fabs(f(a)-f(b))<0.01) break;
+        a=newpt;
     }
     return a;
 }
 
 int main(){
-    cout<<secant(1e5,2e5);
+    cout<<secant(1e5,2e5,1000,1e-6);
 }
