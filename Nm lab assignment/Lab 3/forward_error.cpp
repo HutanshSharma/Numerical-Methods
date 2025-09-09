ebddfd3b95ddf59d07233c3084a57dd810d87589
Hutansh <hutanshsharma241005@gmail.com>
2025-09-10 01:05:49 +0530

    added

--- Nm lab assignment/Lab 3/forward_error.cpp
+++ Nm lab assignment/Lab 3/forward_error.cpp
@@ -1,52 +1,54 @@
-#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-// L2 norm 
-double abs_forward_error(vector<double>calculated_ans, vector<double>real_ans, int size){
-    double sum=0;
-    for(int i=0;i<size;i++){
-        double temp=real_ans[i]-calculated_ans[i];
-        sum+=pow(temp,2);
+// L2 norm
+double norm(vector<double>v1){
+    double sum = 0;
+    for(auto i:v1){
+        sum+=pow(i,2);
     }
-    return pow(sum,0.5);
+
+    return sqrt(sum);
 }
 
-double relative_forward_error(vector<double>calculated_ans, vector<double>real_ans, int size){
-    double sum=0;
-    for(int i=0;i<size;i++){
-        double temp=real_ans[i]-calculated_ans[i];
-        sum+=pow(temp,2);
+double forward_error(vector<double>calans, vector<double>organs){
+    for(int i=0;i<calans.size();i++){
+        calans[i]-=organs[i];
     }
-    double sum_norm=pow(sum,0.5);
-    double val_sum=0;
-    for(auto i:real_ans){
-        val_sum+=pow(i,2);
-    }
-    double org_norm=pow(val_sum,0.5);
-    return sum_norm/org_norm;
+    return norm(calans)/norm(organs);
 }
 
-vector<double> gaussian(vector<vector<double>>&h,int size){
-    for(int i=0;i<size-1;i++){
+vector<double> gaussian(vector<vector<double>>a,int size){
+    for(int i=0;i<size;i++){
+        int maxrowidx = i;
+        double maxvalue = fabs(a[i][i]);
+
+        for(int k=i+1;k<size;k++){
+            if(fabs(a[k][i])>maxvalue){
+                maxrowidx = k;
+                maxvalue = fabs(a[k][i]);
+            }
+        }
+
+        swap(a[maxrowidx],a[i]);
+
         for(int j=i+1;j<size;j++){
-            double factor=h[j][i]/h[i][i];
-            for(int k=i;k<size+1;k++){
-                h[j][k]-=h[i][k]*factor;
+            double factor = a[j][i]/a[i][i];
+            for(int k=i;k<=size;k++){
+                a[j][k] -= a[i][k]*factor;
             }
         }
     }
     vector<double>ans(size);
-    for (int i=size-1;i>=0;i--) {
-        double sum=0.0;
-        for (int j=i+1;j<size;j++) {
-            sum+=h[i][j]*ans[j];
+    for(int i=size-1;i>=0;i--){
+        double sum=0;
+        for(int j=i+1;j<size;j++){
+            sum+=a[i][j]*ans[j];
         }
-        ans[i]=(h[i][size]-sum)/h[i][i];
+        ans[i] = (a[i][size]-sum)/a[i][i];
     }
 
     return ans;
-
 }
 
 int main(){
@@ -60,17 +62,11 @@ int main(){
     }
 
     vector<double>b={11/6.0,13/12.0,47/60.0};
+    vector<double>organs = {1,1,1};
 
     for(int i=0;i<3;i++){
         h[i].push_back(b[i]);
     }
 
-    vector<double>ans=gaussian(h,3);
-    vector<double>actual_ans={1,1,1};
-    cout<<abs_forward_error(ans,actual_ans,3)<<endl;
-    cout<<relative_forward_error(ans,actual_ans,3)<<endl;
-
-    for(auto i:ans){
-        cout<<i<<endl;
-    }
+    cout<<forward_error(gaussian(h,3),organs);
 }
